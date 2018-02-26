0bb3e34bbe59cbb0236251df36cdf589f6a4d205
Joel Sheppard <shepp67@gmail.com>
2018-02-26 12:36:03 -0500

    adding first examples

---
+++ ex1.py
@@ -0,0 +1,6 @@
+#print("Hello World")
+#print("Hello again")
+#print("DaFuk?")
+#print("Printing shit is fun, I suppose")
+#print("this is a new excersize")
+print("don't print this crap")
---
+++ ex2.py
@@ -0,0 +1,10 @@
+# So now we're going to learn comments
+# Pound sign is a comment like bash, etc.
+
+print("I could have code like this.") # and some comment shit here
+
+# You can also use commnts to disable some code
+
+# print("this won't print...")
+
+print("This does print...")
