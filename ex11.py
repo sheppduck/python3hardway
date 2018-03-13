a324ff2363d0281369828b56b035a540dbcedff6
Joel Sheppard <shepp67@gmail.com>
2018-03-13 13:46:31 -0400

    ex11 and 12

---
+++ ex11.py
@@ -0,0 +1,10 @@
+# Example 11 Joel Sheppard
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much to you weigh?", end=' ')
+weight = input()
+
+print(f"According to your responses; you're {age} years old, {height} feet tall, and you weigh {weight} pounds.")
---
+++ ex12.py
@@ -0,0 +1,7 @@
+# Example 12 Joel Sheppard
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much to you weigh? ")
+
+print(f"So According to your answers you're {age} years old, {height} feet tall, and you weigh {weight} pounds.  Is this correct?")
