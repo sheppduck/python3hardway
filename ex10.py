edde7ecfb416ff2befd6b3db43c355407591e2f6
Joel Sheppard <shepp67@gmail.com>
2018-03-13 12:35:01 -0400

    ex 10 updated

--- ex10.py
+++ ex10.py
@@ -2,6 +2,14 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
+## ======== ##
+# Joel's additions
+bell = "Slash A is  \aslashA."
+backspacedout = "So I'm cutting off the las\b tee in last... did it work?"
+reeturn = "Let's see \r what, \r, all these\r \rdo any\rway."
+vert_tab = "What \vIn the \vWorld do\ves a slashv \veven do\v?"
+mixxy = "So \\ what does 'this' look like with the double\\slash then double-quote and finally 'single quotes?'"
+
 fat_cat = """
 I'll do a list:
     \t* Cat Food
@@ -13,4 +21,8 @@ print(tabby_cat)
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
-
+print(bell)
+print(backspacedout)
+print(reeturn)
+print(vert_tab)
+print(mixxy)
