405ed6e0b998ec8cf1bb406bbd5154e71e843d8f
sheppduck <shepp67@gmail.com>
2018-07-30 16:24:46 -0400

    all the things

---
+++ ex24.py
@@ -0,0 +1,42 @@
+print("Let's proactice everything.")
+print('You\'d n eed to know \'bout escapes with \\ that do:')
+print)('\n newlines and \t tabs.')
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+print("--------------")
+print(poem)
+print("--------------")
+
+
+five = 10 - 2 + 3 - 6
+print(f"This should be five: {five}")
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars = jelly_beans / 1000
+    crates = jars / 100
+    return jelly_beans, jars, crates
+
+
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
+
+# Remember that this is another way to format a cooked_string
+print("With a starting point of: {}".format(start_point))
+# its just like with an f"" string
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.".format(*formula))
+
+start_point = start_point / 10
+
+print("We can also do this this way:")
+formula = secret_formula(start_point)
+# this is an easy way toi apply a list to a format string
+print("We'd have ")
