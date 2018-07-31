1dee6f0cdfffd7ed15b4ca8f1501e21cf99e9a1b
sheppduck <shepp67@gmail.com>
2018-07-31 14:57:43 -0400

    putting in more work

--- ex24.py
+++ ex24.py
@@ -1,6 +1,6 @@
-print("Let's proactice everything.")
-print('You\'d n eed to know \'bout escapes with \\ that do:')
-print)('\n newlines and \t tabs.')
+print("Let's practice everything.")
+print('You\'d need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs.')
 
 poem = """
 \tThe lovely world
@@ -32,11 +32,50 @@ beans, jars, crates = secret_formula(start_point)
 # Remember that this is another way to format a cooked_string
 print("With a starting point of: {}".format(start_point))
 # its just like with an f"" string
-print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.".format(*formula))
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
 start_point = start_point / 10
 
 print("We can also do this this way:")
 formula = secret_formula(start_point)
 # this is an easy way toi apply a list to a format string
-print("We'd have ")
+print("We'd have {} beans, {} jars, {} crates.".format(*formula))
+
+#==========================================
+# Additional Practice
+#==========================================
+def break_words(stuff):
+    """This function will break up words for us. """
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    print(word)
+
+def print_last_word(words):
+    """Prints the Last word after popping it off."""
+    word = words.pop(-1)
+    print(word)
+
+def sort_sentence(sentence):
+    """Takes in a full sentence and returns the sorted words."""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of a sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    """Sorts the words and prints the first and last word."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
