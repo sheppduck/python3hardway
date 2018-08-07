7d6a8d343a7c8bf2871cf266c2ea9a344f61ee00
Joel Sheppard <shepp67@gmail.com>
2018-08-06 22:27:11 -0400

    adding ex31 - begin

---
+++ ex31.py
@@ -0,0 +1,7 @@
+# ==========================================
+# Joel Sheppard - learn python3 the hardway
+# Exercise 31
+# ==========================================
+
+print("""You enter a dark room with two doors.
+Do you go through the #1 door or #2 door?""")
