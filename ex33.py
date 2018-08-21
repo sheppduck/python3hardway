d542626ac2746fdcd533ac7738f1ccef2a33961c
Joel Sheppard <shepp67@gmail.com>
2018-08-20 22:40:18 -0400

    just starting drills, off to Zzzzzzzzz

--- ex33.py
+++ ex33.py
@@ -3,18 +3,23 @@
 ## Learn Python3 the Hard way
 # =============================
 
-i = 0
-numbers = []
+# i = 0
+# numbers = []
+#
+# while i < 6:
+#     print(f"At the top i is {i}")
+#     numbers.append(i)
+#
+#     i = i + 1
+#     print("Numbers now: ", numbers)
+#     print(f"At the bottom i is {i}")
+#
+# print("The numbers: ")
+#
+# for num in numbers:
+#     print(num
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
-
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
-
-print("The numbers: ")
-
-for num in numbers:
-    print(num)
+# Convert while loop above to a function that can be called
+# and replace 6 in the i < 6 with a variable
+def prn_loop(my_num):
+    while i 
