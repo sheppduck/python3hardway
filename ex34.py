018936059067b64d89cd83136b20e5111739c0f5
Joel Sheppard <32169940+sheppduck@users.noreply.github.com>
2018-09-04 22:29:09 -0400

    chapt 34 and the study drill.  Damn I surprised myself tonight (#13)

---
+++ ex34.py
@@ -0,0 +1,29 @@
+# =================================
+##  Joel Sheppard - ex34
+## Learn Python3 the Hard way
+## Let's access elements in a list
+# =================================
+animals = ['bear', 'python3.6', 'peacock','kangaroo','whale', 'platypus']
+## Accessing items in the list
+# Cardinally (any random order I want to grab items from list)
+
+cardinal_pickr = animals[0]
+print(f"animal is: {cardinal_pickr}")
+
+# wow I totally guessed the [3 -1] and it flipping worked!
+# Ordinally (in ORDER)
+ordinal_pickr = animals[5 -1]
+print(f"the animal at number is: {ordinal_pickr}")
+
+
+##=======================
+#<< Answers from page 155
+# 1. python3.6
+# 2. peacock
+# 3. bear
+# 4. kangaroo
+# 5. whale
+# 6. peacock
+# 7. platypus
+# 8. whale
+##=======================
